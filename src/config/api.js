ec2e3ed43c1d4247fd72585511a93d9106e09487
jowenzero <jowenzero@gmail.com>
2020-04-26 17:30:03 +0700

    deploy to Netlify

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // set config defaults while creating instance
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://dw15gz2dp-housy.herokuapp.com/api/v1",
 });
 
 // alter config after instance is set
